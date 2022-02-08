13b180a256b2efdb94dc79fd81623a914bb41b51
HelenQuang <qvha.97@gmail.com>
2022-02-08 11:46:32 +0200

    Pages, routes and navbar

---
+++ src/pages/Offers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Offers() {
+  return (
+    <div>
+      <h1>Explore</h1>
+    </div>
+  );
+}
+
+export default Offers;
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SignUp() {
+  return (
+    <div>
+      <h1>Explore</h1>
+    </div>
+  );
+}
+
+export default SignUp;
